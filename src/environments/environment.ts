1667ce4cf22aa41d0532401462e3f5a008d80b74
AyeChanHtunNaing <ayechanhtunnaing@icloud.com>
2022-10-28 15:42:34 +0630

    <AC> added UI draft

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,14 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
+  firebaseConfig : {
+    apiKey: "AIzaSyCHf655KPbnQehMjHXjCUelxeu_RjyNYVU",
+    authDomain: "note-app-2022-3067e.firebaseapp.com",
+    projectId: "note-app-2022-3067e",
+    storageBucket: "note-app-2022-3067e.appspot.com",
+    messagingSenderId: "713766764490",
+    appId: "1:713766764490:web:28010708153fc03d159933"
+  },
   production: false
 };
 
